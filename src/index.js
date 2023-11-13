c6869a08af6eb75c8c0db3295a120b8dd6fba2ae
Teemo Tebest <teemo.tebest@gmail.com>
2023-11-13 17:36:52 +0100

    Updated base script

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import Course01 from './jsx/Course01.jsx';
 import Course02 from './jsx/Course02.jsx';
 
 const containerCourse01 = document.getElementById('app-root-2023-kurssipaketti-course01');
-const rootCourse01 = createRoot(containerCourse01);
-rootCourse01.render(<Course01 />);
+if (containerCourse01) {
+  const rootCourse01 = createRoot(containerCourse01);
+  rootCourse01.render(<Course01 />);
+}
 
 const containerCourse02 = document.getElementById('app-root-2023-kurssipaketti-course02');
-const rootCourse02 = createRoot(containerCourse02);
-rootCourse02.render(<Course02 />);
+if (containerCourse02) {
+  const rootCourse02 = createRoot(containerCourse02);
+  rootCourse02.render(<Course02 />);
+}
