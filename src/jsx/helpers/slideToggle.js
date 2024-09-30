05cedbf7e447c010c732f6c790e6ea4aab33857d
Teemo Tebest <teemo.tebest@gmail.com>
2024-09-30 13:51:17 +0200

    Updated

--- src/jsx/helpers/slideToggle.js
+++ src/jsx/helpers/slideToggle.js
@@ -8,23 +8,10 @@ const slideToggle = (appRef, group, course) => {
     button_container.classList.add('active2');
     container.style.height = 'auto';
 
-    /** Get the computed height of the container. */
-    const height = `${container.clientHeight}px`;
-
-    /** Set the height of the content as 0px, */
-    /** so we can trigger the slide down animation. */
-    container.style.height = '0px';
-
-    /** Do this after the 0px has applied. */
-    /** It's like a delay or something. MAGIC! */
-    setTimeout(() => {
-      container.style.height = height;
-    }, 0);
-    window.dispatchEvent(new Event('resize'));
-
     container.querySelectorAll('.areena_player_container').forEach((el) => {
+      el.classList.remove('areena_player_container');
       const props = {
-        aspectRatio: (course === 'kutsumuskartta') ? '16:9' : '1:1',
+        aspectRatio: ((course === 'kutsumuskartta') ? '16:9' : '1:1'),
         autoplay: false,
         id: el.dataset.id
       };
@@ -36,6 +23,7 @@ const slideToggle = (appRef, group, course) => {
       }
     });
     container.querySelectorAll('.areena_player_container_audio').forEach((el) => {
+      el.classList.remove('areena_player_container_audio');
       const props = {
         autoplay: false,
         id: el.dataset.id,
@@ -48,6 +36,20 @@ const slideToggle = (appRef, group, course) => {
         });
       }
     });
+
+    /** Get the computed height of the container. */
+    const height = `${container.clientHeight}px`;
+
+    /** Set the height of the content as 0px, */
+    /** so we can trigger the slide down animation. */
+    container.style.height = '0px';
+
+    /** Do this after the 0px has applied. */
+    /** It's like a delay or something. MAGIC! */
+    setTimeout(() => {
+      container.style.height = height;
+    }, 200);
+    window.dispatchEvent(new Event('resize'));
 
     /** Slide up. */
   } else {
