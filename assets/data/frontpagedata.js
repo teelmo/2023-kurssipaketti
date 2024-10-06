345c8289064288360712e5c14208d5250c06c616
Antti Saarenpää <antti.saarenpaa@yle.fi>
2024-10-06 11:34:09 +0300

    .

--- assets/data/frontpagedata.js
+++ assets/data/frontpagedata.js
@@ -34,7 +34,7 @@ Tervetuloa mukaan Pieneen erokouluun! Tämä on sinun mahdollisuutesi pysähtyä
     img: '39-12924026655be3ede3f2',
     title: 'Kutsumuskartta',
     text: 'Onko elämä periaatteessa ok, mutta silti tuntuu, että jotain puuttuu? Kutsumuskarttaharjoitus auttaa sinua muistamaan, mitkä asiat tuovat elämääsi iloa – ja löytämään uusia uusia innostuksen kohteita. Oppaana matkalla on hyvän elämän ehtoja tutkinut apulaiprofessori Frank Martela.',
-    link: 'https://aihe.yle.fi/aihe/a/20-10006474',
+    link: 'https://yle.fi/aihe/a/20-10006474',
     wide: true,
     background: `linear-gradient(to top,
       rgba(167, 181, 248, 1) 0%,
@@ -44,7 +44,7 @@ Tervetuloa mukaan Pieneen erokouluun! Tämä on sinun mahdollisuutesi pysähtyä
     img: '39-129740866603b2f17dcf',
     title: 'Keskittyminen',
     text: 'Keskittymiskyky joutuu arjen viriketulvassa valtavalle koetukselle, mutta eipä hätää: sitä voi harjoitella!',
-    link: 'https://aihe.yle.fi/aihe/a/20-10005783',
+    link: 'https://yle.fi/aihe/a/20-10005783',
     background: `linear-gradient(
       0deg,
       rgba(226, 250, 160, 1) 0%,
@@ -55,7 +55,7 @@ Tervetuloa mukaan Pieneen erokouluun! Tämä on sinun mahdollisuutesi pysähtyä
     img: '39-129740766603b2f0d12d',
     title: 'Muistitekniikat',
     text: 'Muistiharjoituksilla opit pitämään ideat tallessa, palauttamaan mieleesi suuriakin kokonaisuuksia ja laajentamaan muistisi kapasiteettia.',
-    link: 'https://aihe.yle.fi/aihe/a/20-10005782',
+    link: 'https://yle.fi/aihe/a/20-10005782',
     background: `linear-gradient(
       to top,
       rgba(245, 140, 73, 1) 0%,
@@ -66,7 +66,7 @@ Tervetuloa mukaan Pieneen erokouluun! Tämä on sinun mahdollisuutesi pysähtyä
     img: '39-129740566603aaa159eb',
     title: 'Rentoutuminen',
     text: 'Vagushermon aktivoiminen saa ylikierroksilla käyvän kehon rentoutumaan ja levottoman mielen rauhoittumaan.',
-    link: 'https://aihe.yle.fi/aihe/a/20-10005785',
+    link: 'https://yle.fi/aihe/a/20-10005785',
     background: `linear-gradient(
       to top,
       rgba(180, 135, 228, 1) 0%,
